1b2776a3e9eae0f17746d5d5b39f4496f5e30d68
lujian <apple60588@vip.qq.com>
2018-04-27 16:10:08 +0800

    1

---
+++ testHibernate/src/com/lj/oneManyDouble/model/Order4.java
@@ -0,0 +1,42 @@
+package com.lj.oneManyDouble.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * onetomany双向-xml
+ * @author lujian
+ * @create 2018年4月27日
+ * @version 1.0
+ */
+public class Order4 {
+	private int id;
+	private String name;
+	private Set<Product4> products = new HashSet<Product4>();
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Set<Product4> getProducts() {
+		return products;
+	}
+	public void setProducts(Set<Product4> products) {
+		this.products = products;
+	}
+		
+	
+}
---
+++ testHibernate/src/com/lj/oneManyDouble/model/Product4.java
@@ -0,0 +1,27 @@
+package com.lj.oneManyDouble.model;
+
+public class Product4 {
+	private int id;
+	private String name;
+	private Order4 order;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Order4 getOrder() {
+		return order;
+	}
+	public void setOrder(Order4 order) {
+		this.order = order;
+	}
+	
+}
