69ee72a931aeac43ac574cc29c4cc50dc032000d
lujian <apple60588@vip.qq.com>
2018-04-27 16:10:30 +0800

    1

--- testHibernate/src/com/lj/oneManyDouble/model/Order4.java
+++ testHibernate/src/com/lj/oneManyDouble/model/Order4.java
@@ -3,11 +3,6 @@ package com.lj.oneManyDouble.model;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
 /**
  * onetomany双向-xml
  * @author lujian
