f56627a953f6e5fac3544eeac2339aab15d62350
lujian <apple60588@vip.qq.com>
2018-05-12 21:07:00 +0800

    1

--- testSpring_mybatis/src/com/lj/mapper/UserAnnotationMapper.java
+++ testSpring_mybatis/src/com/lj/mapper/UserAnnotationMapper.java
@@ -7,12 +7,14 @@ import org.apache.ibatis.annotations.SelectKey;
 import com.lj.model.User;
 
 /**
- * 如何使用mapper代理
+ * 如何使用mapper代理_annotation
  * @author lujian
  * @create 2018年5月9日
  * @version 1.0
  */
 public interface UserAnnotationMapper {
+	//注解相当于把 userMapper.java和userMapper.xml合二为一;但是spring中仍然要配置
+	
 	
 	@Select("select * from mb_user where id = #{id}")
 	User findUser_annotation(int id) throws Exception;
