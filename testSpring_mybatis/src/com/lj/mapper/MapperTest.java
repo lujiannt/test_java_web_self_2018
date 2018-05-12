43d7dcb96f3a47e0c781f97d455c635041649e88
lujian <apple60588@vip.qq.com>
2018-05-12 21:01:24 +0800

    1

--- testSpring_mybatis/src/com/lj/mapper/MapperTest.java
+++ testSpring_mybatis/src/com/lj/mapper/MapperTest.java
@@ -15,11 +15,45 @@ public class MapperTest {
 		applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext.xml");
 	}
 
+	/**
+	 * 普通mapper测试 
+	 * @throws Exception
+	 * @author lujian
+	 * @create 2018年5月12日
+	 */
 	@Test
 	public void test1() throws Exception {
 		UserMapper userMapper = (UserMapper) applicationContext.getBean("userMapper");
 		User user = userMapper.findUserById(2);
 		System.out.println(user.toString());
 	}
-
+	
+	/**
+	 * mapper_annotation测试_查询
+	 * @throws Exception
+	 * @author lujian
+	 * @create 2018年5月12日
+	 */
+	@Test
+	public void test2() throws Exception {
+		UserAnnotationMapper userMapper = (UserAnnotationMapper) applicationContext.getBean("userAnnotationMapper");
+		User user = userMapper.findUser_annotation(1);
+		System.out.println(user.toString());
+	}
+	
+	/**
+	 * mapper_annotation测试_插入
+	 * @throws Exception
+	 * @author lujian
+	 * @create 2018年5月12日
+	 */
+	@Test
+	public void test3() throws Exception {
+		UserAnnotationMapper userMapper = (UserAnnotationMapper) applicationContext.getBean("userAnnotationMapper");
+		User user = new User();
+		user.setUserName("蔡依林");
+		user.setAge(40);
+		userMapper.addUser_annotation(user);
+		System.out.println(user.toString());
+	}
 }
