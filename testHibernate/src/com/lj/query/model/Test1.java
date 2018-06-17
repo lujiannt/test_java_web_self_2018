c06553973e2dbc435f4c0c7b3ee7c053f8d3fa5f
lujian <apple60588@vip.qq.com>
2018-06-17 18:13:47 +0800

    1

--- testHibernate/src/com/lj/query/model/Test1.java
+++ testHibernate/src/com/lj/query/model/Test1.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
